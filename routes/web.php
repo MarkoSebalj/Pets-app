5d25731f9892c08f845ee1fce95a974da579dc11
Marko Sebalj <sabaton123@hotmail.com>
2020-12-10 15:05:02 +0100

    new route v2

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('welcome');    
+})->name('home');
 
 Route::resource('countries', CountryController::class);
 Route::resource('users', UserController::class);
