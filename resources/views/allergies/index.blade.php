ceebcba6df693fef23e8c646b51bf576a207691a
Marko Sebalj <sabaton123@hotmail.com>
2020-12-10 15:42:32 +0100

    allergy buttons route

--- resources/views/allergies/index.blade.php
+++ resources/views/allergies/index.blade.php
@@ -12,7 +12,8 @@
       <th scope="col">Name</th>
       <th scope="col">Category</th> 
       <th scope="col">Note</th>
-      <th scope="col">Diagnosed At</th>     
+      <th scope="col">Diagnosed At</th>
+      <th>Actions</th>    
     </tr>
   </thead>
   <tbody>
@@ -23,7 +24,13 @@
         <td>{{ $allergy->category }}</td>
         <td>{{ $allergy->note }}</td>
         <td>{{ $allergy->diagnosed_at }}</td>
-    @endforeach 
+        <td>          
+          <a class="btn btn-dark" href="{{ route('allergies.show' , ['allergy' => $allergy->id]) }}" role="button">Details</a>
+          <a class="btn btn-dark" href="{{ route('allergies.edit' , ['allergy' => $allergy->id]) }}" role="button">Edit</a>
+        </td>
+      </tr>
+    @endforeach
+
    
   </tbody>
 </table>
